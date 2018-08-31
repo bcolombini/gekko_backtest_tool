2fc5533c56605698c81f158be3753c8112e7c6b0
Bruno Colombini | Stone <bruno.colombini@BRRIOMD011736.local>
2018-08-31 20:44:23 -0300

    f

--- backtests.py
+++ backtests.py
@@ -7,35 +7,34 @@ from collections import namedtuple
 import requests
 import toml
 
-URL_API = "http://buckfolio.com:3000/api/backtest"
+URL = "http://buckfolio.com:3000/api"
+URI_API = URL+"/backtest"
+URI_STRATEGIES = URL+"/strategies"
 DIR_STRATEGIES = "../strategies"
 DIR_TOML_STRATEGIES = "../config/strategies/"
-DATA_SAMPLE = '{"watch":{"exchange":"poloniex","currency":"BTC","asset":"ETH"},"paperTrader":{"feeMaker":0.25,"feeTaker":0.25,"feeUsing":"maker","slippage":0.05,"simulationBalance":{"asset":0.2,"currency":100},"reportRoundtrips":true,"enabled":true},"backtest":{"daterange":{"from":"2018-05-30T22:43:00Z","to":"2018-08-31T00:36:00Z"}},"backtestResultExporter":{"enabled":true,"writeToDisk":false,"data":{"stratUpdates":false,"roundtrips":true,"stratCandles":true,"stratCandleProps":["open"],"trades":true}},"performanceAnalyzer":{"riskFreeReturn":2,"enabled":true},"valid":true}'
-TRADE_ADVISOR = '{"tradingAdvisor": {"enabled": true,"method": "RSI","candleSize": 15,"historySize": 1}}'
+DATA_SAMPLE = 'configurate_data.json'
+TRADE_ADVISOR = 'trading_advisor.json'
 
+def strategies_from_api():
+    toml_dict = dict()
+    strategies = requests.get(URI_STRATEGIES).json()
+    for strategy in strategies:
+        toml_dict.update({strategy['name']:toml.loads(strategy['params'])})
+    return toml_dict
 
 def strategies_names():
     return [f.replace(".js","") for f in listdir(DIR_STRATEGIES) if isfile(join(DIR_STRATEGIES, f)) and ".js" in f]
 
-def parse_toml_file():
-    toml_dict = dict()
-    for strategy_name in strategies_names(): 
-        if isfile(DIR_TOML_STRATEGIES+strategy_name+'.toml'):
-            result = toml.loads(open(DIR_TOML_STRATEGIES+strategy_name+'.toml','r').read())
-            toml_dict.update({strategy_name:result})
-    return toml_dict
-
 def create_post_request():
     
     toml_arr = {}
-    data_advisor = json.loads(TRADE_ADVISOR)
-    data_sample = json.loads(DATA_SAMPLE)
-    all_toml_files = parse_toml_file()
+    data_sample = json.loads(open(DATA_SAMPLE,'r').read())
+    all_toml_files = strategies_from_api()
 
     for json_parsed in all_toml_files:
         toml_dict = dict()
         toml_dict.clear()
-        data_advisor = json.loads(TRADE_ADVISOR)
+        data_advisor = json.loads(open(TRADE_ADVISOR,'r').read())
         data_advisor['tradingAdvisor']['method'] = json_parsed
         toml_dict.update(data_sample)
         toml_dict.update(data_advisor)
@@ -46,12 +45,13 @@ def create_post_request():
 
 def do_request():
     n = create_post_request()
+    print("STRATEGY --- START DATE --- FINISH DATE --- START AMOUNT --- END AMOUNT --- % WIN --- TRADES NUM ")
     for a in n:
         try:
-            q = requests.post(URL_API,json=n[a]).json()
+            q = requests.post(URI_API,json=n[a]).json()
             if 'performanceReport' in q:
-                print(a+" - "+str(q['performanceReport']['relativeProfit']))
+                print(a+" --- "+str(q['performanceReport']['startTime'])+" --- "+str(q['performanceReport']['endTime'])+" --- "+str(q['performanceReport']['startBalance'])+" --- "+str(q['performanceReport']['balance'])+" --- "+str(q['performanceReport']['relativeProfit'])+"% --- "+str(q['performanceReport']['trades']))
         except ValueError:
-            print(ValueError.message)
+            print(a+" --- ")
 
 do_request()
