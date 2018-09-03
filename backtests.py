d8f483d1f36ed2d4aae626a1bc98d58cdde494bd
Bruno Colombini <bruno.colombini@me.com>
2018-09-03 10:31:13 -0300

    Update

--- backtests.py
+++ backtests.py
@@ -1,8 +1,4 @@
-import urllib2
-import urllib
 import json
-from os import listdir
-from os.path import isfile, join
 from collections import namedtuple
 import requests
 import time
@@ -10,17 +6,18 @@ import datetime
 import toml
 import csv
 
-
 URL = "http://buckfolio.com:3000/api"
 URI_API = URL+"/backtest"
 URI_SCANSET = URL+'/scansets'
 URI_STRATEGIES = URL+"/strategies"
 DIR_STRATEGIES = "../strategies"
-DIR_TOML_STRATEGIES = "../config/strategies/"
 DATA_SAMPLE = 'configurate_data.json'
 TRADE_ADVISOR = 'trading_advisor.json'
 TIME_HOUR = 3600
 TIME_DAYS = 24*TIME_HOUR
+WANT_DAYS = 2
+MY_STRATEGIES = ["FIXPRICE","DEBUG_single-advice","x2_rsi","TEMA","RSI_Bull_Bear_Adx_Stop","RSI_BULL_BEAR","RSI_BB_ADX_Peak","RSI","TSI","BBRSI","MK_RSI_BULL_BEAR","RsiStopLoss","bestone_updated_hardcoded","bryanbeck","UO","Supertrend_Gab0","DynBuySell","NEO","TMA","EMACrossover","RSI_BULL_BEAR_ADX_PINGPONG","Dave","tulip-adx","StochRSI","DI","BodhiDI_public","buyatsellat_ui","PPO","DEMACrossover","custom","DEBUG_toggle-advice"]
+
 def str_to_date(ts):
     value = datetime.datetime.fromtimestamp(ts)
     return value.strftime("%Y-%m-%dT%H:%M:%SZ")
@@ -29,21 +26,18 @@ def scan_set():
     obj = []
     all_scan = requests.post(URI_SCANSET,json={}).json()
     for scanned in all_scan['datasets']:
-        obj.append({'coin_inital':scanned['asset'],'date_from':str_to_date(scanned['ranges'][0]['to'] - 7*TIME_DAYS),'date_to':str_to_date(scanned['ranges'][0]['to'])})
+        obj.append({'coin_inital':scanned['asset'],'date_from':str_to_date(scanned['ranges'][0]['to'] - WANT_DAYS*TIME_DAYS),'date_to':str_to_date(scanned['ranges'][0]['to'])})
     return obj
 
 def strategies_from_api():
     toml_dict = dict()
     strategies = requests.get(URI_STRATEGIES).json()
     for strategy in strategies:
-        toml_dict.update({strategy['name']:toml.loads(strategy['params'])})
+        if strategy['name'] in MY_STRATEGIES:
+            toml_dict.update({strategy['name']:toml.loads(strategy['params'])})
     return toml_dict
 
-def strategies_names():
-    return [f.replace(".js","") for f in listdir(DIR_STRATEGIES) if isfile(join(DIR_STRATEGIES, f)) and ".js" in f]
-
 def create_post_request():
-
     toml_arr = {}
     data_sample = json.loads(open(DATA_SAMPLE,'r').read())
     all_toml_files = strategies_from_api()
@@ -72,12 +66,14 @@ def create_post_request():
 
 def do_request():
     n = create_post_request()
-    csvfile = open('backtests.csv', 'w')
+    csvfile = open('backtests_'+str(time.time())+'.csv', 'w')
     fieldnames = ["COIN_NAME","STRATEGY","START DATE","FINISH DATE","START BALANCE","END BALANCE","% WIN","TRADES NUM"]
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
+    i = 0
     for a in n:
         for y in n[a]:
+            i = i+1
             try:
                 q = requests.post(URI_API,json=n[a][y]).json()
                 if 'performanceReport' in q:
@@ -86,13 +82,14 @@ def do_request():
                         "STRATEGY": y,
                         "START DATE":q['performanceReport']['startTime'],
                         "FINISH DATE":q['performanceReport']['endTime'],
-                        "START BALANCE":str(q['performanceReport']['startBalance']),
-                        "END BALANCE":str(q['performanceReport']['balance']),
-                        "% WIN":str(q['performanceReport']['relativeProfit']),
+                        "START BALANCE":str(q['performanceReport']['startBalance']).replace(".",","),
+                        "END BALANCE":str(q['performanceReport']['balance']).replace(".",","),
+                        "% WIN":str(q['performanceReport']['relativeProfit']).replace(".",","),
                         "TRADES NUM":q['performanceReport']['trades']
                     }
                     writer.writerow(row)
+                print(i)
             except ValueError:
-                print(ValueError.message)
+                print(i)
 
 do_request()
